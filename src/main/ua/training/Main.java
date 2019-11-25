fadb3bdaf027017d253105892773d882ebbdb1ae
Oleg Titov <oleggtitoff@gmail.com>
2019-11-25 23:41:22 +0200

    Added controller constructor and implemented main method in Main class.

--- src/main/ua/training/Main.java
+++ src/main/ua/training/Main.java
@@ -1,4 +1,15 @@
 package ua.training;
 
+import ua.training.controller.Controller;
+import ua.training.model.Model;
+import ua.training.view.View;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Controller controller = new Controller(new Model(), new View());
+
+        controller.processUser();
+    }
+
 }
--- src/main/ua/training/controller/Controller.java
+++ src/main/ua/training/controller/Controller.java
@@ -1,4 +1,19 @@
 package ua.training.controller;
 
+import ua.training.model.Model;
+import ua.training.view.View;
+
 public class Controller {
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void processUser() {
+
+    }
+
 }
