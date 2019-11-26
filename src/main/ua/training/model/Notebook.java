0435e20833ddaa0696599634914c947df6d55f73
Oleg Titov <oleggtitoff@gmail.com>
2019-11-26 02:51:34 +0200

    Added Notebook class.

---
+++ src/main/ua/training/model/Notebook.java
@@ -0,0 +1,23 @@
+package ua.training.model;
+
+public class Notebook {
+    String name;
+    String surname;
+    String patronymic;
+    String fullNameShortForm;
+    String nickname;
+    String comment;
+    String group;
+
+    String homePhone;
+    String mobilePhone1;
+    String mobilePhone2;
+    String email;
+    String skype;
+
+    String postIndex;
+    String city;
+    String street;
+    String houseNumber;
+    String apartmentNumber;
+}
