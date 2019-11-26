dc6ac5ed4ce87433e1914b9b7d918e2eab6cea6c
Oleg Titov <oleggtitoff@gmail.com>
2019-11-26 02:11:38 +0200

    Added StringsContainer.

---
+++ src/main/ua/training/view/StringsContainer.java
@@ -0,0 +1,25 @@
+package ua.training.view;
+
+public interface StringsContainer {
+    String EQUAL_SING = "=";
+    String SPACE_SING = " ";
+    String WRONG_INPUT_MESSAGE = "Введено хибні дані! Спробуйте ще раз ";
+    String MESSAGE_OF_THANKS = "Дякуємо, дані збережено. ";
+
+    String NAME_PROMPT = "Введіть ім'я ";
+    String SURNAME_PROMPT = "Введіть прізвище ";
+    String PATRONYMIC_PROMPT = "Введіть ім'я по батькові ";
+    String NICKNAME_PROMPT = "Введіть логін ";
+    String COMMENT_PROMPT = "Введіть коментар ";
+    String GROUP_PROMPT = "Введіть групу ";
+    String HOME_PRONE_PROMPT = "Введіть номер домашнього телефону ";
+    String MOBILE_PHONE_PROMPT = "Введіть номер мобільного телефону ";
+    String OPTIONAL_PHONE_PROMPT = "Введіть номер додаткового мобільного ";
+    String EMAIL_PROMPT = "Введіть E-mail адресу ";
+    String SKYPE_PROMPT = "Введіть ім'я користувача Skype ";
+    String POST_INDEX_PROMPT = "Введіть поштовий індекс ";
+    String CITY_PROMPT = "Введіть назву міста ";
+    String STREET_PROMPT = "Введіть назву вулиці ";
+    String HOUSE_NUMBER_PROMPT = "Введіть номер будинку ";
+    String APARTMENT_NUMBER_PROMPT = "Введіть номер квартири ";
+}
